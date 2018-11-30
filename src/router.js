77a9b3ae42cd58c65b8d12007a61d8cd893b1335
chunge <184364921@qq.com>
2018-11-30 13:55:21 +0800

    add city search

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import City from './views/City.vue'
 
 Vue.use(Router)
 
@@ -13,6 +14,10 @@ export default new Router({
       name: 'home',
       component: Home
         //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }, {
+      path: '/city',
+      name: 'city',
+      component: City
     }
   ]
 })
---
+++ src/views/City.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="city">
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
+</template>
+
+<script>
+import CityHeader from '@/components/city/Header.vue'
+import CitySearch from '@/components/city/Search.vue'
+
+export default {
+    name: "City",
+    components: {
+        CityHeader,
+        CitySearch
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
